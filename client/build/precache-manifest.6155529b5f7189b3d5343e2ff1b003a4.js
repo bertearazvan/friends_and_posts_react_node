a6f815e2fa5fe1a89a287e2e29d66eb0c7d73d3d
bertearazvan <bertea74@gmail.com>
2020-06-01 16:58:36 +0200

    adding5

---
+++ client/build/precache-manifest.6155529b5f7189b3d5343e2ff1b003a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbcb6d37f7c071b5e4c2fe08550e1280",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6bbcd44a5ccb651923ac",
+    "url": "/static/css/main.771e7ce2.chunk.css"
+  },
+  {
+    "revision": "5c96f1a6f25219663eed",
+    "url": "/static/js/2.20fcee1b.chunk.js"
+  },
+  {
+    "revision": "9c04abda40226cf1e9546f90f209d3e0",
+    "url": "/static/js/2.20fcee1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bbcd44a5ccb651923ac",
+    "url": "/static/js/main.5be42b5a.chunk.js"
+  },
+  {
+    "revision": "702554416a81a1e3a894",
+    "url": "/static/js/runtime-main.784c586e.js"
+  }
+]);
